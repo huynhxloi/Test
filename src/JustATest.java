701e9faab8363e204719876cc934c714f59590e1
Loi Huynh <huynhxloi@gmail.com>
2017-10-16 13:47:31 -0500

    Initial commit

---
+++ src/JustATest.java
@@ -0,0 +1,14 @@
+/**
+ * JustATest.java
+ * 
+ * @author Loi Huynh
+ * @version Oct 15, 2017
+ */
+
+public class JustATest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Just a test!");
+    }
+}
